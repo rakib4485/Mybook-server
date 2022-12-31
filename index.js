1c39144d5d778720372425eb0ac9500e61c4e9ed
rakib4485 <mdrakibulislam4485@gmail.com>
2022-12-31 15:09:13 +0600

    profile value updated complete

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efsdsdy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -16,103 +16,145 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try{
-        const allPostsCollection = client
-        .db("Mybook")
-        .collection("allPosts");
+  try {
+    const allPostsCollection = client.db("Mybook").collection("allPosts");
 
-        const usersCollection = client
-        .db("Mybook")
-        .collection("users");
+    const usersCollection = client.db("Mybook").collection("users");
 
-        app.get('/allposts', async(req, res) => {
-            const query = {};
-            const posts = await allPostsCollection.find(query).toArray();
-            res.send(posts);
-        });
+    app.get("/myposts/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const post = await allPostsCollection.find(query).toArray();
+      res.send(post);
+    })
 
-        app.post("/allposts", async (req, res) => {
-            const post = req.body;
-            const result = await allPostsCollection.insertOne(post);
-            res.send(result);
-        });
+    app.get("/allposts", async (req, res) => {
+      const query = {};
+      const posts = await allPostsCollection.find(query).toArray();
+      res.send(posts);
+    });
 
-        app.get('/allposts/:id', async(req, res) =>{
-          const id = req.params.id;
-          const filter = { _id: ObjectId(id) };
-          const result = await allPostsCollection.find(filter).toArray();
-          res.send(result);
-        })
+    app.post("/allposts", async (req, res) => {
+      const post = req.body;
+      const result = await allPostsCollection.insertOne(post);
+      res.send(result);
+    });
 
-        app.put("/allposts/:id", async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) };
-            const post = await allPostsCollection.findOne(filter);
-            const react = await post.react;
-            const newReact = await react + 1;
-            const option = { upsert: true };
-            const updatedDoc = {
-              $set: {
-                react: newReact
-              },
-            };
-            const result = await allPostsCollection.updateOne(
-              filter,
-              updatedDoc,
-              option
-            );
-            res.send(result);
-        });
+    app.get("/allposts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await allPostsCollection.find(filter).toArray();
+      res.send(result);
+    });
 
-        app.get('/allposts/:id/comments', async(req, res) => {
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) };
-            const post = await allPostsCollection.findOne(filter);
-            const comments = await post.comments;
-            res.send(comments);
-        });
+    
 
-        app.put('/allposts/:id/comments', async(req, res) => {
-          const id = req.params.id;
-          const comment = req.body;
-          const filter = { _id: ObjectId(id) };
-          const post = await allPostsCollection.findOne(filter);
-          const comments = post.comments;
-          const newComments = [...comments, comment];
-          const option = { upsert: true };
-            const updatedDoc = {
-              $set: {
-                comments: newComments
-              },
-            };
-            const result = await allPostsCollection.updateOne(
-              filter,
-              updatedDoc,
-              option
-            );
-            res.send(result);
+    app.put("/allposts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const post = await allPostsCollection.findOne(filter);
+      const react = await post.react;
+      const newReact = (await react) + 1;
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          react: newReact,
+        },
+      };
+      const result = await allPostsCollection.updateOne(
+        filter,
+        updatedDoc,
+        option
+      );
+      res.send(result);
+    });
 
-        });
+    app.get("/allposts/:id/comments", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const post = await allPostsCollection.findOne(filter);
+      const comments = await post.comments;
+      res.send(comments);
+    });
 
-        app.post("/users", async (req, res) => {
-          const user = req.body;
-          console.log("user is called")
-          const result = await usersCollection.insertOne(user);
-          res.send(result);
-        });
+    app.put("/allposts/:id/comments", async (req, res) => {
+      const id = req.params.id;
+      const comment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const post = await allPostsCollection.findOne(filter);
+      const comments = post.comments;
+      const newComments = [...comments, comment];
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          comments: newComments,
+        },
+      };
+      const result = await allPostsCollection.updateOne(
+        filter,
+        updatedDoc,
+        option
+      );
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = {
+        email: user.email,
+      };
+      const alreadyAssigned = await usersCollection.find(query).toArray();
+      if (alreadyAssigned.length) {
+        return res.send({ acknowledged: false });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // app.get('/users')
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send(user);
 
 
-    }
-    finally{
+    });
 
-    }
+    app.put('/users/:email', async(req, res) => {
+      const email = req.params.email;
+      const info = req.body;
+
+      const filter = { email: email };
+      const user = await usersCollection.findOne(filter);
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          cover: info.cover,
+          work: info.work,
+          gender: info.gender,
+          school: info.school,
+          college: info.college,
+          status: info.status
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        option
+      );
+    })
+
+  } finally {
+  }
 }
 run().catch(console.log());
 
-app.get('/', (req, res) => {
-    res.send('Mybook Server is Running')
+app.get("/", (req, res) => {
+  res.send("Mybook Server is Running");
 });
 
-app.listen(port, ()=> {
-    console.log(`Mybook serve is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Mybook serve is running on port ${port}`);
+});
